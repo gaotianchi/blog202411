913c8e223c2c20733f58dcef34f76c14a9e7f815
gaotianchi <6159984@gmail.com>
2024-11-21 13:42:47 +0800

    重命名项目文件夹

---
+++ adminservice/src/page/auth/Callback.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="DhFl7ydTf">
+		<span>正在登录...</span>
+	</div>
+</template>
+<script setup lang="ts">
+	import { onMounted } from 'vue';
+	import authservice from '@/service/auth';
+	import { useRouter } from 'vue-router';
+	const router = useRouter();
+	onMounted(async () => {
+		await authservice.userManager.signinCallback();
+		router.push({
+			name: 'TEST',
+		});
+	});
+</script>
+<style lang="css" scoped>
+	.DhFl7ydTf {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 90vh;
+	}
+</style>
---
+++ resourceservice/src/test/java/com/gaotianchi/resourceservice/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaotianchi.resourceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

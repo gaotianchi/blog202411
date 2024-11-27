ed15747e62a15a7227e54a424eaaa3b19917abb0
gaotianchi <6159984@gmail.com>
2024-11-27 15:11:29 +0800

    更新

--- auth/src/main/java/com/gaotianchi/auth/controller/ClientController.java
+++ auth/src/main/java/com/gaotianchi/auth/controller/ClientController.java
@@ -8,7 +8,8 @@ import com.gaotianchi.auth.service.ClientService;
 import com.gaotianchi.auth.vo.ClientVO;
 import com.gaotianchi.auth.vo.VO;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -75,11 +76,21 @@ public class ClientController {
     public VO<String> createClient(@Valid @RequestBody CreateClientDto createClientDto) {
         Client client = fromDtoToClient(createClientDto);
         clientService.insert(client);
-        return VO.response(Code.SUCCESS, "/client/info/" + client.getClientId());
+        return VO.response(Code.SUCCESS, "/client/info/" + client.getId());
     }
 
-    @GetMapping("info")
-    public VO<ClientVO> getInfoById(@RequestParam @NotBlank(message = "客户端ID不能为空") Integer id) {
+    @DeleteMapping("delete/{id}")
+    public VO<Void> deleteClientById(@PathVariable
+                                     @NotNull(message = "id 不能为空")
+                                     @Min(value = 1, message = "id 必须大于等于 1") Integer id) {
+        clientService.deleteById(id);
+        return VO.response(Code.SUCCESS, null);
+    }
+
+    @GetMapping("info/{id}")
+    public VO<ClientVO> getInfoById(@PathVariable
+                                    @NotNull(message = "id 不能为空")
+                                    @Min(value = 1, message = "id 必须大于等于 1") Integer id) {
         Client client = clientService.selectById(id);
 
         ClientVO clientVO = fromClientToClientVO(client);

582d5827eb77274fafac2f5338b7f5354fd048cc
gaotianchi <6159984@gmail.com>
2024-11-27 18:31:49 +0800

    更新

--- auth/src/main/java/com/gaotianchi/auth/entity/Client.java
+++ auth/src/main/java/com/gaotianchi/auth/entity/Client.java
@@ -1,37 +1,38 @@
 package com.gaotianchi.auth.entity;
 
+import java.util.Date;
+
 import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Date;
 
 /**
- * 客户端表(Client)实体类
+ * (Client)实体类
  *
  * @author gaotianchi
- * @since 2024-11-25 13:13:05
+ * @since 2024-11-27 18:31:00
  */
 @Data
 @Builder
 public class Client implements Serializable {
 
     @Serial
-    private static final long serialVersionUID = -87995444269563809L;
+    private static final long serialVersionUID = -79066551719035182L;
 
     private Integer id;
     private String clientId;
     private Date clientIdIssuedAt;
+    private String clientSecret;
+    private Date clientSecretExpiresAt;
     private String clientName;
     private String clientAuthenticationMethods;
+    private String authorizationGrantTypes;
+    private String redirectUris;
+    private String postLogoutRedirectUris;
+    private String scopes;
     private String clientSettings;
     private String tokenSettings;
-    private String authorizationGrantTypes;
-    private String scopes;
-    private String redirectUris;
-    private String clientSecret;
-    private Date clientSecretIssuedAt;
-    private String postLogoutRedirectUris;
 }
 

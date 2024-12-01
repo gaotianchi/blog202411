6a82e8976bdd8d9300f19a0f71bb6a6d64d5cab6
gaotianchi <6159984@gmail.com>
2024-12-01 11:06:10 +0800

    更新

---
+++ auth/src/main/java/com/gaotianchi/auth/startup/config/StartupConfig.java
@@ -0,0 +1,11 @@
+package com.gaotianchi.auth.startup.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author gaotianchi
+ * @since 2024/12/1 11:00
+ **/
+@Configuration
+public class StartupConfig {
+}
---
+++ auth/src/main/java/com/gaotianchi/auth/startup/task/impl/DataInitializationTask.java
@@ -0,0 +1,16 @@
+package com.gaotianchi.auth.startup.task.impl;
+
+import com.gaotianchi.auth.startup.task.StartupTask;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author gaotianchi
+ * @since 2024/12/1 10:52
+ **/
+@Component
+public class DataInitializationTask implements StartupTask {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
